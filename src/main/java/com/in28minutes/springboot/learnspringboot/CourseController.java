24d7c04aff4ad04d0023034126b998924c5255c9
Jisu Kim <kimjs@cyglobal.net>
2025-06-13 12:28:05 +0900

    feat: add Spring Boot DevTools dependency

--- src/main/java/com/in28minutes/springboot/learnspringboot/CourseController.java
+++ src/main/java/com/in28minutes/springboot/learnspringboot/CourseController.java
@@ -13,6 +13,9 @@ public class CourseController {
                   value = "/courses")
   public List<Course> retrieveAllCourses() {
     return Arrays.asList(new Course(1, "Learn AWS", "in28minutes"),
-                         new Course(2, "Learn DevOps", "in28minutes"));
+                         new Course(2, "Learn DevOps", "in28minutes"),
+                         new Course(3, "Learn Azure", "in28minutes"),
+                         new Course(4, "Learn GCP", "in28minutes")
+    );
   }
 }
